945d7485ccba142975fa4e4cbea6bee624b139eb
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-22 16:53:56 -0400

    Adds data-href attr to buttons

--- js/custom-library-card-menu.module.js
+++ js/custom-library-card-menu.module.js
@@ -13,9 +13,14 @@ angular
   }])
   .component('prmLibraryCardMenuAfter', {
     controller: 'customLibraryCardMenuController',
-    template: '<button ng-repeat="item in customLibraryCardMenuItems" aria-label="{{ translate(item.description) }}" ng-click="goToUrl(translate(item.action))" class="button-with-icon zero-margin md-button md-primoExplore-theme md-ink-ripple" type="button">'+
-                '<prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>'+
-                '<span class="customLibraryCardMenuItem">{{ translate(item.name) }}</span>'+
-              '</button>'
+    template: `<button ng-repeat="item in customLibraryCardMenuItems"
+                       aria-label="{{ translate(item.description) }}"
+                       data-href="{{ translate(item.action) }}"
+                       ng-click="goToUrl(translate(item.action))"
+                       class="button-with-icon zero-margin md-button md-primoExplore-theme md-ink-ripple"
+                       type="button">
+                <prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>
+                <span class="customLibraryCardMenuItem">{{ translate(item.name) }}</span>
+              </button>`
 
   });
